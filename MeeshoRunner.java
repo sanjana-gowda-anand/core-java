2987e0981baf759c4ceb8e3438ea44bbd74be18a
sanjana-gowda-anand <sanjana2ananda@gmail.com>
2026-04-05 14:25:59 +0530

    Java Test

--- MeeshoRunner.java
+++
@@ -1,9 +0,0 @@
-class MeeshoRunner {
-    public static void main(String[] args) {
-
-        Meesho m1=new Meesho();
-		Meesho m2=new Meesho();
-		Meesho m3=new Meesho();
-        Meesho m4=new Meesho();
-    }
-}
