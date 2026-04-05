1cdd9f68f8e153cd9453f3b5160538ce74a99df6
sanjana-gowda-anand <sanjana2ananda@gmail.com>
2026-04-05 14:19:36 +0530

    Java Test

---
+++ Laptop.java
@@ -0,0 +1,13 @@
+class Laptop{
+ public static void main(String[] args){
+ 
+ if (10>15)
+ {
+ System.out.println("10 is less than to 15");
+ }else{
+ System.out.println("10 is greater than 15");
+ }
+ 
+
+ }
+}
---
+++ Speaker.java
@@ -0,0 +1,23 @@
+class Speaker {
+
+    static void brand() 
+	{
+        System.out.println("Brand:JBL");
+    }
+
+    static void setVolume(int volume) 
+	{
+        System.out.println("Volume level set to:"+volume);
+    }
+
+    static int price()
+	{
+        return 3500;
+    }
+
+    static String color(String speakerColor)
+	{
+        return speakerColor;
+    }
+
+}
